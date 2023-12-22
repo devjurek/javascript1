b9724409bf7b2db740509a4abc45028af4d6a51b
Jorge Pinto <abgjorgepinto@gmail.com>
2023-12-22 20:37:37 -0300

    Actualizacion de penultima semana del año - Curos Javascript desde cero

--- hi-world.js
+++ hi-world.js
@@ -33,3 +33,81 @@ let diaActual = {Fecha: `hoy es ${new Date}`}
 cambiodefecha(diaActual)
 
 console.log (diaActual)
+
+//probando paso por referencias (arryas)
+let herramientas = ["martillo"]
+herramientas.push("tornillos", "destornillador")
+console.log(herramientas)
+
+//probando paso por referencias (objetos)
+let bolso = {
+    cuaderno: "📗"
+}
+let handbag = bolso
+bolso.handbag = "📓"
+console.log(bolso)
+
+//1. Concatenacion: opcion +
+const direccion = "Calle los espejos 557"
+const ciudad = " Valle cristal"
+const direccionCompleta = "Mi direccion completa es " + direccion + ciudad
+console.log(direccionCompleta)
+
+//2. Concatenacion: Template Literals
+const marca = "Mitsubishi"
+const modelo = "Lancer evolution"
+const año = " 2000"
+const carro = `Este auto es un ${marca} modelo ${modelo} del año ${año}`
+console.log(carro)
+
+//3. Concatenacion: Join()
+const primera = "Me gustaria"
+const segunda = "Ser un buen"
+const tercera = "Ingeniero de Software"
+const pensamiento = [primera, segunda, tercera]
+console.log(pensamiento.join (" "))
+
+//4. concatenacion: concat()
+const hobbie1 = "Correr"
+const hobbie2 = "Gym"
+const hobbie3 = "Programar"
+const hobbies = "Mis hobbies son: ".concat(hobbie1, ", ", hobbie2, ", ", hobbie3, ". " )
+console.log(hobbies)
+
+//5. funciones
+function calculateDiscount (price, discountPercentage) {
+    const discount = (price * discountPercentage) / 100
+    const priceWithDiscount = price - discount
+
+    return priceWithDiscount
+}
+    const originalPrice = 100
+    const discountPercentage = 20
+    const finalPrice = calculateDiscount(originalPrice, discountPercentage)
+
+    console.log("Original Price: $" + originalPrice)
+    console.log("Discount: " + discountPercentage + "%")
+    console.log("Price with discount: $" + finalPrice)
+
+    //6. identificadores
+
+    const house = {
+        dogName: "Fido",
+        dogGreeting: function () {
+            console.log( `Hi, Im ${this.dogName}`)
+        }
+    }
+
+    house.dogGreeting()
+
+    //7. Metodos Bind, call y apply
+
+    const owner = "Manuel"
+    const address = "991 North"
+    const dogName = "Coconut"
+
+    function dogGreeting() {
+        console.log(`Hi, I'm ${dogName} and I live with ${owner} on ${address}`)
+    }
+
+    dogGreeting()
