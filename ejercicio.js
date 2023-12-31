095172702d444cc0314b24e34e507abada09cbdf
Jorge Pinto <abgjorgepinto@gmail.com>
2023-12-31 17:45:17 -0300

    Actualizacion 31.12.23

--- ejercicio.js
+++ ejercicio.js
@@ -137,6 +137,8 @@ performCartActions(VIEW_CART_ACTION)
 
 // el metodo reverse solo coloca los elementos del array en el orden contrario, es necesario ejecutarlo como funcion, tene en cuenta que modifica por completo la constante, si quieres mantener esos valores, deberas crear otra contante.
 
+// actualizacion, es mejor usar el metodo Map
+
 const numbers = [1,2,3,4,5]
 
 const originalNumber = numbers.slice()
@@ -144,3 +146,104 @@ const reverseNumbers = numbers.reverse()
 
 console.log(reverseNumbers)
 console.log(originalNumber)
+
+
+// metodo map y slice
+
+const number = [1,2,3,4,5]
+const newNumber = number.map(num => num * num)
+const newNumberReverse = number.slice().reverse()
+
+console.log(number)
+console.log (newNumber)
+console.log(newNumberReverse)
+
+
+// ejerecicio de convertir una temperatura de fahrenheit a celsius
+
+const tpFarenhenint = [-15, 20, 15]
+const tpCelsius = tpFarenhenint.map( fahrenheit => (5/9) * (fahrenheit-32) )
+
+console.log (tpCelsius)
+console.log (tpFarenhenint)
+
+
+// lo mismo pero sin usar map (menos lineas de codigos, pero solo funciona con un valor)
+
+const tpFarenhenin2t = -15
+const tpCelsius2 = 5/9 * (tpFarenhenint-32)
+
+console.log(tpFarenhenin2t)
+console.log(tpCelsius2)
+
+// suma de arrays con for each
+
+const numb = [1,2,3,4,5]
+let sum = 0
+
+numb.forEach(number => {
+    sum += number
+})
+
+console.log(numb)
+console.log(sum)
+
+
+// metodo filter
+
+const numeroPares = [2,4,6,8,10]
+const numeroFiltrado = numeroPares.filter(numero => numero > 5)
+
+
+console.log (numeroFiltrado)
+
+// metodo reduce
+
+const words = ['apple', 'banana', 'hello', 'bye', 'banana', 'bye', 'bye']
+
+let wordsFrecuency = words.reduce((accumulator, currentValue) => {
+    if (accumulator [currentValue]){
+        accumulator [currentValue]++
+    }
+    else {
+        accumulator [currentValue] = 1
+    }
+    return accumulator
+}, {})
+
+console.log(wordsFrecuency)
+
+// Excersice: Ganador de una rifa
+
+const winningParticipants = [
+    {id: 1, name: "Unojose", ticketNumber:001},
+    {id: 22, name: "Dosnielk", ticketNumber:0331},
+    {id: 333, name: "Tresfijo", ticketNumber:0121},
+    {id: 4444, name: "Cuatronale", ticketNumber:0351},
+]
+
+function findWinnerByname (name) {
+    const winner = winningParticipants.find(participants => participants.name === name)
+    return winner || "No winner find with that name"
+}
+
+function findIndexWinnerByTicket (ticketNumber){
+    const index = winningParticipants.findIndex(participants => participants.ticketNumber === ticketNumber)
+    return index !== -1? index : "No winner find with that ticket number"
+}
+
+function displayWinnerInformation(winner){
+    if (winner !== undefined && winner != null && winner !== "No winner found with tha ticket number") {
+        console.log("Winnet information ", winner)
+    }
+}
+
+const winnerByName = findWinnerByname("Dosnielk")
+const indexWinnetByTicket = findIndexWinnerByTicket(015)
+
+displayWinnerInformation(winnerByName)
+console.log("Index of Winner by Ticket Number ", indexWinnetByTicket)
+
+
+
+
